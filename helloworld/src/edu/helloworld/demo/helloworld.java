1053558140997003ea84d98bb289c1fbeb6d447f
pjha7 <pjha7@PoojaPC>
2024-05-23 10:14:08 +0530

    first commit
    Signed-off-by: pjha7 <pjha7@PoojaPC>

---
+++ helloworld/src/edu/helloworld/demo/helloworld.java
@@ -0,0 +1,11 @@
+package edu.helloworld.demo;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
